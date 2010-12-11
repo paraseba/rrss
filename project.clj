5149d7b11a855bae89f33fbfea4f4e2867ae4529
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2010-12-11 16:49:04 -0200

    better project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject rrss "0.1.0-SNAPSHOT"
   :description "Ring Redis Session Store"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :url "https://github.com/paraseba/rrss"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
+  :dependencies [;[org.clojure/clojure "1.2.0"]
                  [ring/ring-core "0.3.5"]
-                 [redis.clients/jedis "1.5.0"]]
-            )
-  ;:dev-dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]]
-  ;:repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
-                 ;"stuartsierra-snapshots" "http://stuartsierra.com/m2snapshots"})
+                 [redis.clients/jedis "1.5.0"]])
