5605cde514b5db4908b798eed2e242a7f9bfacce
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2010-12-11 19:17:25 -0200

    optimization, don't create new fn all the time

--- src/rrss.clj
+++ src/rrss.clj
@@ -4,10 +4,10 @@
   (:import java.util.UUID)
   (:use [ring.middleware.session.store :only (SessionStore)]))
 
-(defn- with-connection [pool f]
+(defn- with-connection [pool f & args]
   (let [connection (.getResource pool)]
     (try
-      (f connection)
+      (apply f connection args)
       (finally (when connection
                  (.returnResource pool connection))))))
 
@@ -20,44 +20,41 @@
   (let [string-map (into {"__" ""} (map #(vec (map as-str %)) data))]
     (.hmset connection key string-map)))
 
-(defn- read-session* [config key]
-  (fn [connection]
-    (if (nil? key)
-      {}
-      (let [m (.hgetAll connection ((config :map-key) key))]
-        (dissoc (into {} m) "__")))))
+(defn- read-session* [connection config key]
+  (if (nil? key)
+    {}
+    (let [m (.hgetAll connection ((config :map-key) key))]
+      (dissoc (into {} m) "__"))))
 
-(defn- delete-session* [config key]
-  (fn [connection]
-    (when-not (nil? key)
-      (.del connection (into-array String [((config :map-key) key)])))
-    nil))
+(defn- delete-session* [connection config key]
+  (when-not (nil? key)
+    (.del connection (into-array String [((config :map-key) key)])))
+  nil)
 
-(defn- write-session* [config key data]
+(defn- write-session* [connection config key data]
   (let [key (or key (str (UUID/randomUUID)))]
-    (fn [connection]
-      (hmset connection ((config :map-key) key) data)
-      key)))
-
-(deftype RedisStore [pool config]
-  SessionStore
-  (read-session [_ key]
-    (with-connection pool (read-session* config key)))
-
-  (write-session [_ key data]
-    (with-connection pool (write-session* config key data)))
-
-  (delete-session [_ key]
-    (with-connection pool (delete-session* config key))))
+    (hmset connection ((config :map-key) key) data)
+    key))
 
 (defn- add-prefix [prefix]
   (fn [key] (str prefix key)))
 
+(deftype RedisStore [pool config]
+  SessionStore
+  (read-session [_ key]
+    (with-connection pool read-session* config key))
+
+  (write-session [_ key data]
+    (with-connection pool write-session* config key data))
+
+  (delete-session [_ key]
+    (with-connection pool delete-session* config key)))
+
 (defn redis-store
   ([] (redis-store {}))
-  ([{:keys (host port map-key)
-     :or {host "localhost" port 6379 map-key (add-prefix "sessions:")}}]
+  ([{:keys (host port map-key hooks)
+     :or {host "localhost" port 6379 map-key (add-prefix "sessions:") :hooks {}}}]
    (let [pool (JedisPool.  (org.apache.commons.pool.impl.GenericObjectPool$Config.)
                           host
                           port)]
-     (RedisStore. pool {:map-key map-key}))))
+     (RedisStore. pool {:map-key map-key :hooks hooks}))))
