d14db91e80a54b28e19e67c62414b963fe119070
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2010-12-11 23:55:45 -0200

    some doc

--- src/rrss.clj
+++ src/rrss.clj
@@ -1,4 +1,6 @@
 (ns rrss
+  "Ring Redis Session Store
+  Create a Ring session store, implemented on top of Redis key/value store"
   (:import org.apache.commons.pool.impl.GenericObjectPool$Config)
   (:import redis.clients.jedis.JedisPool)
   (:require rrss.store)
@@ -8,11 +10,26 @@
 (defn- add-prefix [prefix]
   (fn [key] (str prefix key)))
 
+(def ^{:private true} default-options
+  {:host "localhost"
+   :port 6379
+   :map-key (add-prefix "sessions:")
+   :hooks []}) 
+
+(defn- pool-config [options]
+  (org.apache.commons.pool.impl.GenericObjectPool$Config.))
+
 (defn redis-store
+  "Create the session store.
+  Valid options are:
+    :host redis server URL string
+    :port redis server port
+    :map-key a function that takes a key string and returns the key that
+    will be saved to redis. Defaults to  foo -> sessions:foo
+    :hooks vector of hooks that will be used. See rrss.hooks"
+
   ([] (redis-store {}))
-  ([{:keys (host port map-key hooks)
-     :or {host "localhost" port 6379 map-key (add-prefix "sessions:") :hooks []}}]
-   (let [pool (JedisPool.  (org.apache.commons.pool.impl.GenericObjectPool$Config.)
-                          host
-                          port)]
+  ([options]
+   (let [{:keys (host port map-key hooks)} (merge default-options options)
+         pool (JedisPool. (pool-config options) host port)]
      (RedisStore. pool {:map-key map-key :hook (compose-hooks hooks)}))))
