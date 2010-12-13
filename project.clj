cb3d931235be540032c451816b5bc540a7c47641
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2010-12-13 14:26:14 -0200

    depend on clojure 1.2.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject rrss "0.2.0-SNAPSHOT"
+(defproject rrss "0.2.1-SNAPSHOT"
   :description "Ring Redis Session Store"
   :url "https://github.com/paraseba/rrss"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
-  :dependencies [;[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
                  [ring/ring-core "0.3.5"]
                  [redis.clients/jedis "1.5.0"]])
